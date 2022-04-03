55e5a9522461acd55241692da2cc242ac624bcae
Hermawan <hermawangan39@gmail.com>
2022-04-03 23:38:08 +0700

    style implemented

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -5,44 +5,54 @@ import basketDelete from "./images/icon-delete.svg";
 
 function Cart({ price, number, disc, click, setClick }) {
   const [discountPrice, setDiscountPrice] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     const discountPrice = +disc * price;
+    const totalPrice = discountPrice * number;
+
+    setTotalPrice(totalPrice.toFixed(2));
     setDiscountPrice(discountPrice);
-  }, [click, disc, price]);
+  }, [click, disc, price, number]);
 
   const clickDeleteHandler = () => {
     setClick(false);
   };
 
   return (
-    <>
+    <div className=" absolute   bg-white border-2 border-solid w-11/12 h-2/5 md:h-1/3 top-14 left-4 rounded-md md:w-1/3 lg:left-1/2 md:left-2/3 md:top-16">
+      <h4 className="text-bold border-b-2 border-solid p-4 font-bold">Cart</h4>
       {click ? (
-        <div>
-          <img src={thumbnail} alt="thumbnail" />
+        <>
+          <div className="flex justify-between items-center p-4">
+            <img src={thumbnail} alt="thumbnail" className="w-1/6 rounded" />
 
-          <div>
-            <div>
+            <div className="text-Neutral-DarkGrayishBlue">
               <p>Fall Limited Edition Sneakers</p>
-              <h2>${discountPrice.toFixed(2)}</h2>
-              <span>X {number} </span>{" "}
-              <span>${number * discountPrice.toFixed(2)}</span>
+              <span>${discountPrice.toFixed(2)}</span>
+              <span> x {number} </span>{" "}
+              <span className="font-bold">${totalPrice}</span>
             </div>
             <div>
               <img
                 src={basketDelete}
                 alt="dusbin"
                 onClick={clickDeleteHandler}
+                className="cursor-pointer"
               />
             </div>
           </div>
 
-          <button>Checkout</button>
-        </div>
+          <button className="border-2 border-primary-Orange w-11/12 mx-4 text-white bg-primary-Orange rounded-lg hover:text-primary-Orange hover:bg-white h-10 ">
+            Checkout
+          </button>
+        </>
       ) : (
-        <p>Your cart is empty.</p>
+        <p className="text-center text-Neutral-DarkGrayishBlue pt-20">
+          Your cart is empty.
+        </p>
       )}
-    </>
+    </div>
   );
 }
 
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,60 +3,115 @@ import logo from "./images/logo.svg";
 import cart from "./images/icon-cart.svg";
 import avatar from "./images/image-avatar.png";
 import Cart from "./Cart";
+import { FaBars } from "react-icons/fa";
 
 function Nav({ click, setClick }) {
   const [mouseOver, setMouseOver] = useState(false);
   const [burgerClick, setBurgerClick] = useState(false);
 
   const mouseHover = () => {
-    setMouseOver(true);
+    setMouseOver(!mouseOver);
   };
 
-  const mouseLeave = () => {
-    setMouseOver(false);
+  const clickHandler = () => {
+    setBurgerClick(!burgerClick);
   };
 
   return (
-    <>
-      <div className=" flex justify-between">
-        <div className=" flex md:flex-row flex-col">
-          <img src={logo} alt="brand logo" className="w-1/2 h-1/2" />
-          <div className="bg-white">
-            <ul className="flex justify-between md:flex-row flex-col ">
-              <li className="p-6">
-                <a href="#">Collections</a>
+    <nav className=" border-solid border-b-2 flex md:justify-center justify-between h-12">
+      <div className="flex justify-between lg:w-2/3 w-full">
+        <div className=" flex md:flex-row flex-col  w-10/12  ">
+          <div className="flex  justify-start items-center h-full">
+            <button
+              className="md:hidden
+             px-2"
+              onClick={clickHandler}
+            >
+              <FaBars />
+            </button>
+            <img src={logo} alt="brand logo" className="w-1/2 h-1/2  px-2" />
+          </div>
+          <div
+            className={` bg-white  md:block border-2 border-solid md:border-none  ${
+              burgerClick ? "block absolute h-full w-2/3" : "hidden"
+            } md:static md:w-7/12 `}
+          >
+            {burgerClick ? (
+              <button
+                onClick={clickHandler}
+                className={`${burgerClick ? "block" : "hidden"} md:hidden`}
+              >
+                X
+              </button>
+            ) : null}
+            <ul className="flex justify-between md:flex-row flex-col  md:h-full md:items-center ">
+              <li className=" ">
+                <a
+                  href="#"
+                  className="hover:border-b-primary-Orange hover:border-b-2 md:pb-3"
+                >
+                  Collections
+                </a>
               </li>
-              <li className="p-6">
-                <a href="#">Men</a>
+              <li className="">
+                <a
+                  href="#"
+                  className="hover:border-b-primary-Orange hover:border-b-2 md:pb-3"
+                >
+                  Men
+                </a>
               </li>
-              <li className="p-6">
-                <a href="#">Women</a>
+              <li className="">
+                <a
+                  href="#"
+                  className="hover:border-b-primary-Orange hover:border-b-2 md:pb-3"
+                >
+                  Women
+                </a>
               </li>
-              <li className="p-6">
-                <a href="#">About</a>
+              <li className="">
+                <a
+                  href="#"
+                  className="hover:border-b-primary-Orange hover:border-b-2 md:pb-3"
+                >
+                  About
+                </a>
               </li>
-              <li className="p-6">
-                <a href="#">Contact</a>
+              <li className="">
+                <a
+                  href="#"
+                  className="hover:border-b-primary-Orange hover:border-b-2 md:pb-3"
+                >
+                  Contact
+                </a>
               </li>
             </ul>
           </div>
         </div>
 
-        <div>
-          <img src={cart} alt="cart icon" onMouseOver={mouseHover} />
+        <div className=" flex justify-around items-center">
+          <img
+            src={cart}
+            alt="cart icon"
+            onClick={mouseHover}
+            className="h-7 cursor-pointer"
+          />
           {click ? <span>1</span> : null}
-          <img src={avatar} alt="user avatar" />
+          <img
+            src={avatar}
+            alt="user avatar"
+            className="w-4/12 cursor-pointer hover:border-primary-Orange hover:border-2 rounded-full"
+          />
         </div>
         <>
           {mouseOver ? (
-            <div>
-              <button onClick={mouseLeave}>X</button>
+            <>
               <Cart click={click} setClick={setClick} />
-            </div>
+            </>
           ) : null}
         </>
       </div>
-    </>
+    </nav>
   );
 }
 
